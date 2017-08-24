ba67593828856ab3a08a70d7ae101b13216f646f
Mazter707 <mazter707@gmail.com>
2017-08-23 20:01:01 -0700

    Initial commit

---
+++ app/src/main/java/com/platzi/plantilla_menu_hamburguer/Views/Activitys/PlantillaApp.java
@@ -0,0 +1,19 @@
+package com.platzi.plantilla_menu_hamburguer.Views.Activitys;
+
+import android.app.Application;
+
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
+/**
+ * Created by USER on 23/08/2017.
+ */
+
+public class PlantillaApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
+    }
+}
