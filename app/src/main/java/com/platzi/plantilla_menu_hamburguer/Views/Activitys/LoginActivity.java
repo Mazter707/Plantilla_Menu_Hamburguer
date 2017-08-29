e81e7c303763dccffab35d78ef1b769fee9f6d2d
Mazter707 <mazter707@gmail.com>
2017-08-28 18:55:36 -0700

    Hamburguer  menu code review.

--- app/src/main/java/com/platzi/plantilla_menu_hamburguer/Views/Activitys/LoginActivity.java
+++ app/src/main/java/com/platzi/plantilla_menu_hamburguer/Views/Activitys/LoginActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
-
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
@@ -12,10 +11,7 @@ import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 import com.platzi.plantilla_menu_hamburguer.R;
 
-
-
 public class LoginActivity extends AppCompatActivity {
-
     private LoginButton loginButton;
     private CallbackManager callbackManager;
 
@@ -23,18 +19,12 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
         callbackManager = CallbackManager.Factory.create();
-
         loginButton = (LoginButton) findViewById(R.id.loginButton);
-
-
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
-
-                goMainScreen();
-
+                goToMainActivity();
             }
 
             @Override
@@ -49,18 +39,16 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 
-    private void goMainScreen() {
-        Intent intent = new Intent(this, StartActivity.class);
+    private void goToMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(intent);
-
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         callbackManager.onActivityResult(requestCode, resultCode, data);
-
     }
 }
 
